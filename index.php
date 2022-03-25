7f9273bef8796ededb7cfb5153fe7c3f0780f4c6
Rezza1245 <76737279+Rezza1245@users.noreply.github.com>
2022-03-25 11:35:42 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<?php
+session_start();
+# jika saat load halaman ini, pastikan telah login sbg user
+if (!isset($_SESSION["user"])) {
+    header("location:login.php");
+}
+include "navbar.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <h1>
+        <div>
+            <img width="100%" src="piting.jpg" alt="foto">
+        </div>
+    </h1>
+</body>
+</html>
